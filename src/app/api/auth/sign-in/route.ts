51cfc30e0ccfbf7469383415097c72e6656681d9
Tushar Rawat <tusharrawat52@gmail.com>
2024-02-07 02:30:14 +0530

    feat: add auth API

---
+++ src/app/api/auth/sign-in/route.ts
@@ -0,0 +1,66 @@
+import prismadb from "@/lib/prismadb";
+import { SignInSchema } from "@/validators/auth";
+import { NextRequest, NextResponse } from "next/server";
+import bcrypt from 'bcrypt'
+import jwt from 'jsonwebtoken'
+
+
+export async function POST(request : NextRequest) {
+    try {
+        const body = await request.json();
+        const parsedInput = SignInSchema.safeParse(body);
+
+        if(!parsedInput.success) {
+            return NextResponse.json({
+                ok : false,
+                msg : "input validation failed",
+                error : parsedInput.error.issues[0].message
+            } , {status : 501})
+        }
+
+        // check is there exist an user by this email
+        const existingUserByEmail = await prismadb.user.findUnique({
+            where : {
+                email : body.email
+            }
+        })
+
+        if(!existingUserByEmail || existingUserByEmail.hashedPassword === null) {
+            return NextResponse.json({
+                ok : false,
+                msg : "email doesn't exist",
+            } , {status : 501})
+        }
+
+        const isUserValid = await bcrypt.compare(body.password , existingUserByEmail.hashedPassword);
+
+        if(!isUserValid) {
+            return NextResponse.json({
+                ok : false,
+                msg : "password is incorrect"
+            } , {status : 501})
+        }
+
+        const jwtToken = jwt.sign({_id : existingUserByEmail.id} ,String( process.env.JWT_SECRET) , {expiresIn : '24h'});
+
+        return NextResponse.json({
+            ok : true,
+            msg : "successful login",
+            jwtToken : jwtToken,
+            user : {
+                name : existingUserByEmail.name,
+                id : existingUserByEmail.id,
+                email : existingUserByEmail.email,
+                isWalletActivated : existingUserByEmail.walletActivated
+            }
+        } , {status : 201})
+        
+    } catch (error) {
+        console.log(error);
+        return NextResponse.json({
+            ok : false,
+            msg : 'error in sign-in',
+            error : error
+        } , {status  : 403})
+    }
+}
--- src/app/api/auth/sign-up/route.ts
+++ src/app/api/auth/sign-up/route.ts
@@ -1,18 +1,82 @@
+import prismadb from "@/lib/prismadb";
 import {  SignUpSchema } from "@/validators/auth";
 import { NextRequest , NextResponse } from "next/server";
+import bcrypt from 'bcrypt'
 
 
-export async function POST(request: NextRequest) {
+export async function POST(request: NextRequest , {params} : any) {
      try {
-        const body = request.body;       
-        console.log(body);
+       const body =  await request.json();
+       const parsedInput = SignUpSchema.safeParse(body);
 
-        return NextResponse.json({
-            body
-        })
+       // validation
+       if(!parsedInput.success){
+         return NextResponse.json({
+            ok : false ,
+            msg : "data validation error",
+            error : parsedInput.error
+         } , {status : 500})
+       }
+
+       // check if there is an existing user
+       const existingUserByEmail = await prismadb.user.findUnique({
+         where : {
+            email : body.email
+         }
+       })
+
+       if(existingUserByEmail){
+         return NextResponse.json({
+            ok : false,
+            msg : "Email already exist",
+         } , {status : 501})
+       }
+
+       const existingUserByPhoneNO = await prismadb.user.findUnique({
+         where : {
+            phoneNo : body.phoneNo
+         }
+       })
+
+       if(existingUserByPhoneNO) {
+         return NextResponse.json({
+            ok : false,
+            msg : "Phone NO. already exist"
+         } , {status : 501})
+       }
+
+
+       // Now , create a new user
+
+       const hashedPassword = await bcrypt.hash(body.password , 12);
+
+       const newUser = await prismadb.user.create({
+           data : {
+             name : body.name,
+             email : body.email,
+             phoneNo : body.phoneNo,
+             gender : body.gender,
+             hashedPassword : hashedPassword,
+             walletActivated : false
+           }
+       })
+
+       return NextResponse.json({
+         ok : true,
+         msg : "new user created",
+         user : {
+            name : newUser.name,
+            id : newUser.id
+         }
+       } , {status : 201})
+
    
      } catch (error) {
         console.log("error in sign-up" , error);
-        return new NextResponse("error in sign-up" , {status : 500});
+        return NextResponse.json({
+          ok : false,
+          msg : "error in sign-up",
+          error
+        } , {status : 401})
      }
 }
